f4f37434f5c578f2538e5ad1ee4b75d121f2be3f
momdadok <christin.lai+github@gmail.com>
2016-08-07 22:16:49 -0400

    Add files via upload

---
+++ banbury_mill_sep.R
@@ -0,0 +1,11 @@
+counter<-2
+L2temp_plotdata$startup<-1
+
+for(i in 2:dim(banbury_peak)[1]){
+    if(banbury_peak$cycle_time[i]>210){
+      index_val<-match(banbury_peak$time[i],L2temp_plotdata$time)
+      L2temp_plotdata$startup[index_val:dim(L2temp_plotdata)[1]]<-counter
+    counter<-counter+1
+  }
+}
+
